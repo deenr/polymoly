5f747b59c509b855440f27229d4b3a8c46a78e4e
Dean Reymen <deenrayman@outlook.com>
2023-03-04 23:27:00 +0100

    Card flip implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,9 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  cardFlipped = false;
-  rotateX: number;
-  rotateY: number;
-  @ViewChild('document') document: ElementRef;
-
-  @HostListener('mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    this.rotateX =
-      -(this.document.nativeElement.offsetWidth / 2 - event.pageX) / 20;
-    this.rotateY =
-      (this.document.nativeElement.offsetHeight / 2 - event.pageY) / 10;
-  }
+  isCardFlipped = false;
 
   flipCard(): void {
-    this.cardFlipped = !this.cardFlipped;
+    this.isCardFlipped = !this.isCardFlipped;
   }
 }
--- src/app/poly-card/poly-card.component.ts
+++ src/app/poly-card/poly-card.component.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostListener,
   Input,
+  SimpleChanges,
   ViewChild,
 } from '@angular/core';
 
@@ -12,7 +13,34 @@ import {
   styleUrls: ['./poly-card.component.scss'],
 })
 export class PolyCardComponent {
-  @Input() cardFlipped: boolean;
-  @Input() rotateX: number;
-  @Input() rotateY: number;
+  @Input() isCardFlipped: boolean;
+  @ViewChild('card') card: ElementRef;
+  mouseNotOnCard = false;
+  rotateX: number;
+  rotateY: number;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const { left, top } = this.card.nativeElement.getBoundingClientRect();
+    this.rotateX =
+      -(this.card.nativeElement.offsetWidth / 2 - (event.pageX - left)) / 20;
+    this.rotateY =
+      (this.card.nativeElement.offsetHeight / 2 - (event.pageY - top)) / 10;
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave() {
+    this.mouseNotOnCard = true;
+    this.rotateX = 0;
+    this.rotateY = 0;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      'isCardFlipped' in changes &&
+      !changes['isCardFlipped'].isFirstChange()
+    ) {
+      this.card.nativeElement.classList.toggle('is-flipped');
+    }
+  }
 }
